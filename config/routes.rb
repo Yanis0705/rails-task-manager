12ae97ee5fa70a132cde5a4a37af314142d29597
Yanis Chaigneau <yanis.chaigneau4@orange.fr>
2019-08-06 14:57:14 +0200

    Update RailsTaskManager

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
-  get 'tasks', to: 'tasks#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :tasks
+
+  # # READ
+  # get 'tasks', to: 'tasks#index'
+
+  # # CREATE
+  # get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  # post 'tasks', to: 'tasks#create'
+
+  # # READ BIS
+  # get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  # # UPDATE
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # patch 'tasks/:id',    to: 'tasks#update'
+
+  # #DELETE
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
